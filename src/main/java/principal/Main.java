36bba2a3f11474fa2a8f8625f45ebc8bf79f8905
ChocolateKest <gustavoemartins@gmail.com>
2025-06-21 20:00:23 -0300

    First commit

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -6,12 +6,12 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        double a = Double.parseDouble(JOptionPane.showInputDialog("Digite o os minutos utilizados neste mês:"));
+        double minutos = Double.parseDouble(JOptionPane.showInputDialog("Digite o os minutos utilizados neste mês:"));
 
-        if(a <= 100){
+        if(minutos <= 100){
             JOptionPane.showMessageDialog(null,"Valor á pagar é R$ 50.00");
         }else{
-            double b =  ((a - 100) * 2) + 50;
+            double b =  ((minutos - 100) * 2) + 50;
             JOptionPane.showMessageDialog(null,"Valor á pagar é: " + b);
 
         }
